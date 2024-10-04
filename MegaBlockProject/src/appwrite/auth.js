7a20417cde2654b72385665b57ec88ba604adc87
AkshayKothule <akshaykothule2002@gmail.com>
2024-10-04 20:19:51 +0530

    work on Authentications

---
+++ MegaBlockProject/src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import confi from '../confi.js'
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client=new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(confi.appwriteUrl)
+            .setProject(confi.appwriteProjectId)
+        this.account=new Account(this.client);
+    }
+    async createAccount({email , password  , name}){
+        try{
+            const userAccount=await this.account.create(ID.unique() , email , password , name);
+
+            if(userAccount){
+                //call another method
+                return this.login({email , password})
+            }else{
+                return userAccount;
+            }
+
+        }catch(error){
+            throw error;
+        }
+
+    }
+
+    async login({email , password}){
+        try{
+            return await this.account.createEmailPasswordSession(email , password);
+
+
+        }catch(error){
+            throw error;
+
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            //logged in 
+            return await this.account.get()
+
+        }catch(error){
+            console.log("Appwrite service :: getCurrentUser :: error" , error);
+
+        }
+
+        return null;
+    }
+    async logOut(){
+        try{
+             await this.account.deleteSessions();
+
+        }catch(error){
+            console.log("Appwrite service :: logOut :: error" , error);
+
+        }
+    }
+}
+
+const authService=new AuthService();
+export default authService
--- MegaBlockProject/src/confi/confi.js
+++ MegaBlockProject/src/confi/confi.js
@@ -1,6 +1,6 @@
 const confi={
     appwriteUrl:strin(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectid:string(import.meta.env.VITE_PEOJECT_ID),
+    appwriteProjectId:string(import.meta.env.VITE_PEOJECT_ID),
     appwriteDatabaseId:string(import.meta.env.VITE_DATABASE_ID),
     appwriteCollectionId:string(import.meta.env.VITE_COLLECTION_ID),
     appwriteBucketId:string(import.meta.env.VITE_BUCKET_ID)
